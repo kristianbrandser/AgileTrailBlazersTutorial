b211321e32b883038e5944d7e50ced41073942fb
Kristian Brandser <kristian.brandser@gmail.com>
2017-06-16 17:27:06 +0200

    http://www.agiletrailblazers.com/blog/the-5-step-guide-for-selenium-cucumber-and-gherkin

---
+++ features/step_definitions/search_steps.rb
@@ -0,0 +1,21 @@
+require 'selenium-webdriver'
+driver = Selenium::WebDriver.for :chrome
+
+Given(/^We navigate to the homepage$/) do
+  driver.navigate.to "http://www.google.com/"
+end
+
+When(/^We search for the word agile$/) do
+  driver.find_element(:id, 's').send_keys("agile")
+  driver.find_element(:id, 'submit-button').click
+end
+
+Then(/^The results for the search will be displayed$/) do
+  wait = Selenium::WebDriver::Wait.new(:timeout => 5) # seconds
+  begin
+    element = wait.until { driver.find_element(:id => "search-title") }
+    expect(element.text).to eq('Search Results for: agile')
+  ensure
+    driver.quit
+  end
+end
---
+++ search.feature
@@ -0,0 +1,5 @@
+Feature: Site Search
+Scenario:
+  Given We navigate to the homepage
+  When We search for the word agile
+  Then The results for the search will be displayed
